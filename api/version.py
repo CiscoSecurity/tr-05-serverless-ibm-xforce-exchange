fbfc0aafdb484254bea8847d2d10f5f1fdd81160
Ian Redden <iaredden@cisco.com>
2021-01-27 12:36:46 -0500

    remove old code directories

--- api/version.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, current_app, jsonify
-
-version_api = Blueprint('version', __name__)
-
-
-@version_api.route('/version', methods=['POST'])
-def version():
-    return jsonify({'version': current_app.config['VERSION']})
